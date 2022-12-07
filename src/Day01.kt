b429c8f1081be07100d761ac31e44646b616a658
tstellfe <tstellfe@icloud.com>
2022-12-07 08:49:27 +0100

    day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,26 +1,38 @@
 fun main() {
-  fun part1(input: List<String>): Int {
+  fun List<String>.toSummedList(): MutableList<Int> {
     val summed: MutableList<Int> = mutableListOf()
     var temp = 0
-    input.forEach {
+    this.forEach {
       if (it != "") temp += it.toInt()
       else {
         summed.add(temp)
         temp = 0
       }
     }
+    summed.add(temp)
+    return summed
+  }
 
-    return summed.max()
+  fun part1(input: List<String>): Int {
+    return input.toSummedList().max()
   }
 
   fun part2(input: List<String>): Int {
-    return input.size
+    val mutableSummed = input.toSummedList()
+    var top3 = 0
+    (1..3).forEach { _ ->
+      val index = mutableSummed.indexOf(mutableSummed.max())
+      top3 += mutableSummed[index]
+      mutableSummed.removeAt(index)
+    }
+
+    return top3
   }
 
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("Day01_test")
 
   val input = readInput("Day01")
-  println(part1(testInput))
-  println(part1(input))
+  println(part2(testInput))
+  println(part2(input))
 }
